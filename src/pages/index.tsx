256027d00302fd22f0a0854cecdd3fca8caad9a8
hota1024 <hotalog@hota1024.com>
2020-11-19 23:23:42 +0900

    ✨ Add image view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from 'react'
+import SocketIO from 'socket.io-client'
+
 export default function Home(): React.ReactElement {
+  const [image, setImage] = useState<string>()
+
+  useEffect(() => {
+    const io = SocketIO('http://localhost:8000')
+
+    io.on('image', (data: ArrayBuffer) => {
+      setImage(`data:image/png;base64,${Buffer.from(data).toString('base64')}`)
+    })
+  }, [])
+
   return (
     <>
-      <div>hello</div>
+      <div className="container">
+        <img className="image" src={image} />
+      </div>
+
+      <style global jsx>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          background: #eeeeff;
+        }
+
+        .container {
+          min-height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .image {
+          border-radius: 4px;
+          box-shadow: 0 0 16px rgba(0, 0, 0, 0.3);
+          max-width: 640px;
+        }
+      `}</style>
     </>
   )
 }
