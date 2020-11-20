c94bccfd5ba3fd125897b4f7e8c9b82dac169384
hota1024 <hotalog@hota1024.com>
2020-11-20 17:48:52 +0900

    💄 Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  font-size: 0.95rem;
+  background: #00aaee;
+  color: white;
+  min-width: 100px;
+  height: 32px;
+  cursor: pointer;
+  transition: all 200ms ease-in-out;
+
+  :hover {
+    background: #0088cc;
+  }
+`
